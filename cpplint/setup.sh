a3619df465354a5dbdd57144955cd6e9cc340678
xxmawhu <xxmawhu@163.com>
2019-08-22 20:12:06 +0800

    rm cpplint

--- cpplint/setup.sh
+++
@@ -1,2 +0,0 @@
-CpplinDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-alias CheckCpp='python ${CpplinDIR}/checkAndmodify.py '
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export PATH=/afs/ihep.ac.cn/soft/common/python27_sl65/bin:$PATH
+#export PATH=/afs/ihep.ac.cn/soft/common/python27_sl65/bin:$PATH
 BaskDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${BaskDIR}:$PYTHONPATH
 source ${BaskDIR}/cpplint/setup.sh
