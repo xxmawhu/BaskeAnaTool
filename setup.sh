8e8b2fc0faaa51dc230b772ff06f5ebdb85cebcf
xxmawhu <xxmawhu@163.com>
2019-08-23 12:52:27 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #export PATH=/afs/ihep.ac.cn/soft/common/python27_sl65/bin:$PATH
 BaskDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${BaskDIR}:$PYTHONPATH
-source ${BaskDIR}/cpplint/setup.sh
+source /besfs/users/lihb/software/SL6/python-2.7.16/thispython.sh
 source ${BaskDIR}/short/setup.sh
 source ${BaskDIR}/SimAndRec/setup.sh
