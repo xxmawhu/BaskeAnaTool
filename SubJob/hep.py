8f12cdfa5c37f30ce4ba58dc8c95bed5c87b207e
xxmawhu <xxmawhu@163.com>
2019-10-08 13:09:41 +0800

    import mulprocess, use 20 processers to sub the jobs

--- SubJob/hep.py
+++ SubJob/hep.py
@@ -1,99 +1,43 @@
-#!/afs/ihep.ac.cn/soft/common/python27_sl65/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : Xin-Xin MA
+#   Email         : xxmawhu@163.com
+#   File Name     : hep.py
+#   Created Time  : 2019-10-08 11:07
+#   Last Modified : 2019-10-08 11:07
+# ====================================================
 import os
 import progressbar
 from commands import getoutput
+import subprocess
+from multiprocessing import Pool
+import util
 #hep_sub = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics"
-def Sub(files,
-        subcommand = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics",
-        Type='.sh', logID=''):
-    if len(files) == 0:
-        print("No Job found!!!")
-        return
-    list.sort(files)
-    print("Sub %d jobs......"%(len(files)))
-    for i in progressbar.progressbar(range(len(files))):
-        JOB = os.path.split(files[i])
-        getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
-        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        #print('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        if logID!='':
-            f=open(logID,  'a')
-            f.write(out.split()[-1]+'\n')
-            f.close()
-    print("Sub All Jobs Successful!!!")
-
-# sub one job
-def smartSubOneJob(File, logID='.log'):
-    hep_sub = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics"
-    JOB = os.path.split(File)
-    #sub the job
-    if JOB[1].split('.')[-1] == 'txt':
-        subcommand = 'boss.condor'
-        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-    elif JOB[1].split('.')[-1] == 'sh':
-        out = getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
-        subcommand = hep_sub
-        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        #print('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-    elif JOB[1].split('.')[-1] in ['C', 'cxx', 'cc', 'cpp']:
-        shName = mkBash(File, 'root -l -b -q')
-        JOB = os.path.split(shName)
-        subcommand = hep_sub
-        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-    elif JOB[1].split('.')[-1] == 'py':
-        shName = mkBash(files[i], 'python')
-        JOB = os.path.split(shName)
-        subcommand = 'hep_sub -g physics'
-        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        #print out
-    if logID!='':
-        f=open(logID,  'a')
-        f.write(out.split()[-1]+'\n')
-        f.close()
-
-#sub job list
-def smartSub(files, logID='.log'):
-    hep_sub = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics"
-    if len(files) == 0:
-        print("No Job found!!!")
-        return
-    list.sort(files)
-    print("Sub %d jobs......"%(len(files)))
-    for i in progressbar.progressbar(range(len(files))):
-        # path / file
-        JOB = os.path.split(files[i])
-        #print JOB
-        #sub the job
-        out = ""
-        if JOB[1].split('.')[-1] == 'txt':
-            subcommand = 'boss.condor'
-            out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        elif JOB[1].split('.')[-1] == 'sh':
-            subcommand = hep_sub
-            getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
-            out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        elif JOB[1].split('.')[-1] in ['C', 'cxx', 'cc', 'cpp']:
-            shName = mkBash(files[i], 'root -l -b -q')
-            JOB = os.path.split(shName)
-            subcommand = hep_sub
-            getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
-            out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        elif JOB[1].split('.')[-1] == 'py':
-            shName = mkBash(files[i], 'python')
-            JOB = os.path.split(shName)
-            subcommand = hep_sub
-            getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
-            out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
-        #print out
-        if logID!='':
-            f=open(logID,  'a')
-            f.write(out.split()[-1]+'\n')
-            f.close()
+def SubOneJob(file_sub):
+    """
+    Args:
+        fil_sub -> list, 
+        file name: [0]
+        sub command: [1]
+    return
+        job id
+    """
+    file_name = file_sub[0]
+    sub_command = file_sub[1]
+    JOB = os.path.split(file_name)
+    getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
+    out = getoutput('cd %s; %s %s'%(JOB[0], sub_command, JOB[1]))
+    return out.split()[-1]
 
 
 def mkBash(afile, command="root -l -b -q"):
-    #print afile
+    """
+       make a bash file, write:
+        command afile
+    """
     Path=os.path.split(afile)[0]
     File=os.path.split(afile)[1]
     name=os.path.splitext(File)[0]
@@ -107,8 +51,148 @@ def mkBash(afile, command="root -l -b -q"):
     getoutput('chmod +x ' + bashNm)
     return bashNm
 
-#generate Bash file
+
+def smartSubOneJob(file_name):
+    """
+    determine the sub command by the file type
+    return
+        string: the id
+    """
+    sub_command = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics"
+
+    #sub the BOSS job
+    file_type = file_name.split('.')[-1]
+    if file_type == "":
+        print "NameError: can't determine the type of this file from name"
+        print "\t >>{}".format(file_name)
+        return ''
+    if file_type == 'txt':
+        sub_command="/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/boss.condor"
+        file_sub = [file_name, sub_command]
+        return SubOneJob(file_sub)
+
+    if file_type in ['sh', 'csh']:
+        file_sub = [file_name, sub_command]
+        return SubOneJob(file_sub)
+
+    # make the Bash File, then sub
+    try:
+        exe_command = util.exe[file_type]
+        sh_name = mkBash(file_name, exe_command)
+        file_sub = [sh_name, sub_command]
+    except KeyError as e:
+        print "KeyError: no such type in util.exe"
+        raise e
+    return SubOneJob(file_sub)
+
+
+def SubJobList(files, sub_command):
+    """
+    Args:
+        files->list, the abspath of each job
+        sub_command->list, the type of each job
+    return -> list
+        job id list
+    """
+    if not files:
+        print("Warning::No Job found!!!")
+        return
+
+    list.sort(files)
+    print("Sub %d jobs......"%(len(files)))
+    file_sub_list = [[i, sub_command] for i in files]
+    idList = []
+    pool = Pool(20)
+    for i in progressbar.progressbar(range(len(files)/20)):
+        # 20 processers perform best!
+        ids = pool.map(SubOneJob, file_sub_list[i*20:(i+1)*20])
+        for ID in ids:
+            idList.append(ID)
+    
+    print("Sub All Jobs Successful!!!")
+    return idList
+
+
+def Sub(files, Types = [],
+        sub_command = "hep_sub"):
+    """
+    Args:
+        files: list, the abspath of each job
+        Types: list, the type of each job
+    """
+    if not files:
+        print("Warning::No Job found!!!")
+        return
+
+    print("Sub %d jobs......"%(len(files)))
+    list.sort(files)
+    file_sub_list = [[i, sub_command] for i in files]
+    idList = []
+    for i in progressbar.progressbar(range(len(files)/20)):
+        pool = Pool(20)
+        ids = pool.map(SubOneJob, file_sub_list[i*20:(i+1)*20])
+        for ID in ids:
+            idList.append(ID)
+    
+    print("Sub All Jobs Successful!!!")
+    return idList
+
+
+def Sub(files,
+        subcommand = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub",
+        Type='.sh', logID=''):
+    if not files:
+        print("No Job found!!!")
+        return
+    list.sort(files)
+    print("Sub %d jobs......"%(len(files)))
+    for i in progressbar.progressbar(range(len(files)/20)):
+        JOB = os.path.split(files[i])
+        pool = Pool(20)
+        getoutput('cd %s; %s %s'%(JOB[0], "chmod +x", JOB[1]))
+        out = getoutput('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
+        #print('cd %s; %s %s'%(JOB[0], subcommand, JOB[1]))
+        if logID!='':
+            f=open(logID,  'a')
+            f.write(out.split()[-1]+'\n')
+            f.close()
+    print("Sub All Jobs Successful!!!")
+
+
+def smartSub(files):
+    """
+    Args:
+        files->list, the abspath of each job
+    return -> list
+        job id list
+    """
+    if not files:
+        print("Warning::No Job found!!!")
+        return
+    list.sort(files)
+    print("Sub %d jobs......"%(len(files)))
+    idList = []
+    pool = Pool(20)
+    for i in progressbar.progressbar(range(len(files)/20)):
+        # 20 processers perform best!
+        ids = pool.map(smartSubOneJob, files[i*20:(i+1)*20])
+        for ID in ids:
+            idList.append(ID)
+    pool.close()
+    
+    print("Sub All Jobs Successful!!!")
+    return idList
+
+
 def genBashList(files, command='root -l -b -q'):
+    """
+    Args:
+        files->list, 
+        command->string, how to execute one job
+    return->list
+        one list filled with the name of bash file associated 
+        with each input file
+    """
     jobs = []
     for i in files:
         jobs.append(mkBash(i, command))
@@ -116,8 +200,15 @@ def genBashList(files, command='root -l -b -q'):
     return jobs
 
 def SubBash(jobs, logID='.log'):
+    """
+    Args:
+        jobs->list, the bash jobs
+    return->list
+        the job ID list
+    """
     hep_sub = "/afs/ihep.ac.cn/soft/common/sysgroup/hep_job/bin/hep_sub -g physics"
-    Sub(jobs, hep_sub, logID)
+    sub_command = [hep_sub for i in jobs]
+    return SubJobList(jobs, sub_command)
 
 def SubBOSS(jobs, logID='.log'):
     Sub(jobs, 'boss.condor', logID)
