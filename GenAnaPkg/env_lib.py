205875b184af1995175a288dc920692188d0fece
SyrakuShaikh <spin.hk@outlook.com>
2019-10-30 18:29:15 +0800

    fix for not removing '...ROOT=' prefix.

--- GenAnaPkg/env_lib.py
+++ GenAnaPkg/env_lib.py
@@ -5,9 +5,9 @@
 # Author:       Hao-Kai SUN
 # Created:      2019-10-29 Tue 16:19:50 CST
 # <<=====================================>>
-# Last Updated: 2019-10-30 Wed 18:23:39 CST
+# Last Updated: 2019-10-30 Wed 18:29:06 CST
 #           By: Hao-Kai SUN
-#     Update #: 114
+#     Update #: 116
 # <<======== COPYRIGHT && LICENSE =======>>
 #
 # Copyright © 2019 SUN Hao-Kai <spin.hk@outlook.com>. All rights reserved.
@@ -72,8 +72,9 @@ fullLIBP: list = []
 temp: list = []
 for l in LIBP:
     hasso: bool = False
-    temp.append(l)
-    for fp in list(pl.Path(l).rglob('lib*.so')):
+    lp: str = ''.join(l.split('=')[1:])
+    temp.append(lp)
+    for fp in list(pl.Path(lp).rglob('lib*.so')):
         hasso = True
         libn: str = fp.resolve(strict=True).name[3:-3]
         temp.append(object)
