8ae91979abeee7dd50eb0eb55294a23fb0882419
xxmawhu <xxmawhu@163.com>
2019-12-30 22:34:53 +0800

    update

---
+++ SimAndRec/Ana.py
@@ -0,0 +1,28 @@
+#!/afs/ihep.ac.cn/soft/common/python27_sl65/bin/python
+# -*- coding: utf-8 -*-
+class ana():
+    def __init__(self, opt='', dst='', out=''):
+        self._opt = opt
+        self._dst = dst
+        self._out = out
+
+    def Make(self, ff):
+        f = open(ff, 'w')
+        s = self._opt
+        s += '''
+ApplicationMgr.EvtMax = -1; 
+
+// Set output level threshold (2=DEBUG, 3=INFO, 4=WARNING, 5=ERROR, 6=FATAL )
+MessageSvc.OutputLevel = 5;
+
+ApplicationMgr.HistogramPersistency = "ROOT";
+
+EventCnvSvc.digiRootInputFile = {
+    "''' + self._dst + '''"
+};
+NTupleSvc.output={
+    "FILE DATAFILE = ' ''' + self._out + '''' OPT = 'new' TYP = 'ROOT'"
+};
+'''
+        f.write(s)
+        f.close()
