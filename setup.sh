e8e4fff095d9cc79a427c10ad097249cc1c95278
xxmawhu <xxmawhu@163.com>
2020-01-01 18:41:45 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #export PATH=/afs/ihep.ac.cn/soft/common/python27_sl65/bin:$PATH
 # status="$(git pull)"
+cwd="$(pwd)"
 BaskDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${BaskDIR}:$PYTHONPATH
 source /besfs/users/lihb/software/SL6/python-2.7.16/thispython.sh
 source ${BaskDIR}/short/setup.sh
 cd ${BaskDIR}/SimAndRec 
 python addSimor.py
-cd ${BaskDIR}
+cd ${cwd}
 source ${BaskDIR}/SimAndRec/setup.sh
 
 #use this python
